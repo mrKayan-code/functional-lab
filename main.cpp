6deaa2eae26b66768cc6cadfefcb3e3e885307de
mrkayan <mrkayan999@gmail.com>
2026-04-24 10:11:32 +0700

    parseDate

--- main.cpp
+++ main.cpp
@@ -10,6 +10,13 @@ struct  Date
     int d;
 };
 
+Date parseDate(string stringDate) { // YYYY-MM-DD
+    int y = stoi(stringDate.substr(0, 4));
+    int m = stoi(stringDate.substr(5, 2));
+    int d = stoi(stringDate.substr(8, 2));
+    
+    return {y, m, d};
+}
 
 struct Order {
     int id;
