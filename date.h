dffe4a76fe11fa26f5fe204d95963f72a5a466e8
mrkayan <mrkayan999@gmail.com>
2026-04-24 17:55:53 +0700

    fix std usings in date.h

--- date.h
+++ date.h
@@ -1,13 +1,15 @@
 #pragma once
 #include <string>
 
+using namespace std;
+
 struct Date {
     int year;
     int month;
     int day;
 };
 
-Date parseDate(const std::string& stringDate);
+Date parseDate(const string& stringDate);
 bool isDateOlder(const Date& dateA, const Date& dateB);
 bool isDateInRange(const Date& date, const Date& start, const Date& end);
-std::string dateToString(const Date& date);
+string dateToString(const Date& date);
