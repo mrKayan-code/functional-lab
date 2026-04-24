d386590981302b83ecd2f0109dbd8d70f64f6e0a
mrkayan <mrkayan999@gmail.com>
2026-04-24 17:54:29 +0700

    fix references in date.h and some fix biuld tasks

--- date.cpp
+++ date.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-Date parseDate(string stringDate) {
+Date parseDate(const string& stringDate) {
     int y = stoi(stringDate.substr(0, 4));
     int m = stoi(stringDate.substr(5, 2));
     int d = stoi(stringDate.substr(8, 2));
@@ -20,7 +20,7 @@ bool isDateInRange(const Date& date, const Date& start, const Date& end) {
     return !isDateOlder(date, start) && !isDateOlder(end, date);
 }
 
-string dateToSring(const Date& date) {
+string dateToString(const Date& date) {
     string monthString = (date.month < 10) ? ("0" + to_string(date.month)) : to_string(date.month);
     string dayString = (date.day < 10) ? ("0" + to_string(date.day)) : to_string(date.day);
     return to_string(date.year) + "-" + monthString + "-" + dayString;
--- main.cpp
+++ main.cpp
@@ -6,17 +6,7 @@
 using namespace std;
 
 int main() {
-    string input = "2024-03-10";
-    Date d = parseDate(input);
-
-    cout << "Parsed: " << dateToString(d) << endl;
-
-
-    Date start = {2024, 1, 1};
-    Date end   = {2024, 12, 31};
-    bool inRange = isDateInRange(d, start, end);
     
-    cout << (inRange ? "In range\n" : "Out of range\n");
 
     return 0;
 }
